1f211ccce2b10ca7fcfbfbfeb98bfa694ef7b6dd
Neon <51766327+DeveloperNeon@users.noreply.github.com>
2021-05-17 15:24:42 -0500

    Create installuf.sh

---
+++ installuf.sh
@@ -0,0 +1,12 @@
+echo " "
+echo " "
+echo "Installing File Uploader Script by NeonDevelopment."
+echo " "
+echo " "
+cd /bin/
+wget https://sh.neonthe.dev/uf.sh
+mv uf.sh uf
+chmod 777 /bin/uf
+echo " "
+echo " "
+echo "Neon's File Uploader Script has been installed. Run uf to use it."
