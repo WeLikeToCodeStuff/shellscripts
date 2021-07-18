42777d98c0f3d91f811885563c107edc90e21346
Neon <51766327+DeveloperNeon@users.noreply.github.com>
2021-07-18 09:16:42 -0500

    Update teleport.sh

--- teleport.sh
+++ teleport.sh
@@ -3,7 +3,7 @@ curl https://deb.releases.teleport.dev/teleport-pubkey.asc | sudo apt-key add -
 add-apt-repository 'deb https://deb.releases.teleport.dev/ stable main' 
 apt-get update 
 apt install teleport
-wget fileurl
+wget https://raw.githubusercontent.com/WeLikeToCodeStuff/shellscripts/main/teleport.service
 mv teleport.service /etc/systemd/system/teleport.service
 systemctl daemon-reload
 systemctl enable teleport
