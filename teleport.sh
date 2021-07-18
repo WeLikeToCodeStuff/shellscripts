9f73cb07ccdfe1780862c0445f0b93b55655b490
Neon <51766327+DeveloperNeon@users.noreply.github.com>
2021-07-18 09:26:30 -0500

    Update teleport.sh

--- teleport.sh
+++ teleport.sh
@@ -7,7 +7,8 @@ wget https://raw.githubusercontent.com/WeLikeToCodeStuff/shellscripts/main/telep
 mv teleport.service /etc/systemd/system/teleport.service
 if [ -z "$1" ]
 then
-systemctl daemon-reload
-systemctl enable teleport
+  systemctl daemon-reload
+  systemctl enable teleport
 else
-teleport start    --roles=node    --token=$1    --ca-pin=sha256:3e53d54d97c6d8a12f2f720179a80b4087093f6d5cd00ae3054367643f6a565a    --auth-server=104.206.82.28:3025
+  teleport start    --roles=node    --token=$1    --ca-pin=sha256:3e53d54d97c6d8a12f2f720179a80b4087093f6d5cd00ae3054367643f6a565a    --auth-server=104.206.82.28:3025
+fi
