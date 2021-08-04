833a736bfb0cef795c291a51f3e257267bbbe1cd
Neon <51766327+DeveloperNeon@users.noreply.github.com>
2021-08-04 09:11:53 -0500

    Update uf.sh

--- uf.sh
+++ uf.sh
@@ -2,10 +2,10 @@ if [ -z "$1" ]
 then
       echo "Please retype the command with the following example: uf /path/to/file.ext filename.ext"
 else
-      RESULT="`wget --method PUT -O - -nv --body-file=$1 http://transfersh.com/$2`"
+      RESULT="`wget --method PUT -O - -nv --body-file=$1 http://transfer.sh/$2`"
       echo ""
       echo ""
       echo ""
       SUBSTRING=$(echo $RESULT| cut -d'/' -f 4)
-      echo "Uploaded file: http://transfersh.com/get/$SUBSTRING/$2"
+      echo "Uploaded file: http://transfer.sh/get/$SUBSTRING/$2"
 fi
